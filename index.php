41855f847e9cf5d3d15a7c4dbf5ddc591e4d1ff3
GillesRitchmond <51457330+GillesRitchmond@users.noreply.github.com>
2022-10-28 20:26:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 
 <body>
 <div class="mobile">
+<iframe class="airtable-embed" src="https://airtable.com/embed/shrdOUmZkbtUTtOkn?backgroundColor=blue" frameborder="0" onmousewheel="" width="100%" height="533" style="background: transparent; border: 1px solid #ccc;"></iframe>
 
     <div class="first-part-content">
         <nav class="navbar navbar-light bg-light">
